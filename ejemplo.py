71e6d491faf411d121bbded58ab05c314850f5ff
ferchosour <fercho05.fso@gmail.com>
2025-09-06 16:57:14 -0600

    Add files via upload
    En vez de la función de costo MSE he usado la función Cross -Entropy

--- ejemplo.py
+++ ejemplo.py
@@ -21,6 +21,17 @@ archivo_lectura = open("red_prueba.pkl",'rb')
 net = pickle.load(archivo_lectura)
 archivo_lectura.close()
 
+# Debido a que hemos actualizado la función de costo, al llamar el código
+# network, este código no está reconociendo a la función de costo nueva
+# Cross Entropy, por lo que debemos asegurarnos de que la red contenga
+# a la función, así pues la importamos de nuestro otro código
+from network import CrossEntropyCost
+if not hasattr(net, "cost"):   # Pues el error que nos sale anteriormente
+    # es que no tiene el atributo cost
+    net.cost = CrossEntropyCost # Así llamamos a nuestra nueva función como
+    # la anterior para no alterar más el código
+
+#Ahora tenemos que entrenar la red de nuevo
 net.SGD( training_data, 15, 50, 0.5, test_data=test_data)
 
 archivo = open("red_prueba.pkl",'wb')
